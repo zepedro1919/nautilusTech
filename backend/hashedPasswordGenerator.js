bc733bd13bc724435ffe258c1676a9af263d211d
zepedro1919 <130983842+zepedro1919@users.noreply.github.com>
2025-02-19 12:56:54 +0000

    Removed backend

--- backend/hashedPasswordGenerator.js
+++
@@ -1,6 +0,0 @@
-const bcrypt = require("bcryptjs");
-
-const newPassword = "cfonautilus"; // The password you want to set
-bcrypt.hash(newPassword, 10).then((hash) => {
-  console.log("New Hashed Password:", hash);
-});
