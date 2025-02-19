2a3fe1fcd87e79d9d9f1b94c14d1f66d76bb2f6e
zepedro1919 <130983842+zepedro1919@users.noreply.github.com>
2025-02-19 14:08:52 +0000

    Delete src directory

--- src/components/protectedRoute.js
+++
@@ -1,8 +0,0 @@
-import { Navigate, Outlet } from "react-router-dom";
-
-const ProtectedRoute = () => {
-    const token = localStorage.getItem("token");
-    return token ? <Outlet /> : <Navigate to="/authentication/sign-in" replace />;
-};
-
-export default ProtectedRoute;
